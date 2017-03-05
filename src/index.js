437d19bae09822a0e4358d442feef574f7c70663
bokuweb <bokuweb12@gmail.com>
2017-03-05 12:43:20 +0900

    Add expected items

--- src/index.js
+++ src/index.js
@@ -35,130 +35,131 @@ module.exports = ({
   dist,
   ignoreError,
 }: Props) => new Promise((resolve, reject) => {
-  let spinner = new Spinner('[Processing].. %s');
-  spinner.setSpinnerString('|/-\\');
-  spinner.start();
-  const expectedImages = glob.sync(`${expectedDir}${IMAGE_FILES}`)
-    .map(path => path.replace(expectedDir, ''));
-  const actualImages = glob.sync(`${actualDir}${IMAGE_FILES}`)
-    .map(path => path.replace(actualDir, ''));
-  const deletedImages = difference(expectedImages, actualImages);
-  const newImages = difference(actualImages, expectedImages);
+    let spinner = new Spinner('[Processing].. %s');
+    spinner.setSpinnerString('|/-\\');
+    spinner.start();
+    const expectedImages = glob.sync(`${expectedDir}${IMAGE_FILES}`)
+      .map(path => path.replace(expectedDir, ''));
+    const actualImages = glob.sync(`${actualDir}${IMAGE_FILES}`)
+      .map(path => path.replace(actualDir, ''));
+    const deletedImages = difference(expectedImages, actualImages);
+    const newImages = difference(actualImages, expectedImages);
 
-  mkdirp.sync(expectedDir);
-  mkdirp.sync(diffDir);
+    mkdirp.sync(expectedDir);
+    mkdirp.sync(diffDir);
 
-  const compareAndGenerateDiff = (
-    actualDir: string,
-    expectedDir: string,
-    diffDir: string,
-    image: string,
-  ): Promise<CompareResult> => {
-    return new Promise((resolve, reject) => {
-      imageDiff({
-        actualImage: `${actualDir}${image}`,
-        expectedImage: `${expectedDir}${image}`,
-        diffImage: `${diffDir}${image}`,
-        shadow: true,
-      }, (err, imagesAreSame) => {
-        if (err) reject(err);
-        resolve({ passed: imagesAreSame, image });
+    const compareAndGenerateDiff = (
+      actualDir: string,
+      expectedDir: string,
+      diffDir: string,
+      image: string,
+    ): Promise<CompareResult> => {
+      return new Promise((resolve, reject) => {
+        imageDiff({
+          actualImage: `${actualDir}${image}`,
+          expectedImage: `${expectedDir}${image}`,
+          diffImage: `${diffDir}${image}`,
+          shadow: true,
+        }, (err, imagesAreSame) => {
+          if (err) reject(err);
+          resolve({ passed: imagesAreSame, image });
+        })
       })
-    })
-  };
+    };
 
-  const compareImages = (
-    expectedImages: string[],
-    actualImages: string[]
-  ): Promise<$TupleMap<CompareResult[], typeof $await>> => {
-    return Promise.all(actualImages.map((actualImage) => {
-      if (!expectedImages.includes(actualImage)) return;
-      return compareAndGenerateDiff(
-        actualDir,
-        expectedDir,
-        diffDir,
-        actualImage,
-      )
-    }).filter(p => !!p))
-  };
+    const compareImages = (
+      expectedImages: string[],
+      actualImages: string[]
+    ): Promise<$TupleMap<CompareResult[], typeof $await>> => {
+      return Promise.all(actualImages.map((actualImage) => {
+        if (!expectedImages.includes(actualImage)) return;
+        return compareAndGenerateDiff(
+          actualDir,
+          expectedDir,
+          diffDir,
+          actualImage,
+        )
+      }).filter(p => !!p))
+    };
 
-  const cleanupExpectedDir = () => {
-    expectedImages.forEach((image) => fs.unlinkSync(`${expectedDir}${image}`));
-  };
+    const cleanupExpectedDir = () => {
+      expectedImages.forEach((image) => fs.unlinkSync(`${expectedDir}${image}`));
+    };
 
-  const copyImages = () => {
-    actualImages.forEach((image) => {
-      try {
-        mkdirp.sync(path.dirname(`${expectedDir}${image}`));
-        fs.createReadStream(`${actualDir}${image}`)
-          .pipe(fs.createWriteStream(`${expectedDir}${image}`));
-      } catch (err) {
-        log.fail(err);
-      }
-    })
-  };
-
-  if (deletedImages.length > 0) {
-    log.warn(`\n\u274B ${deletedImages.length} deleted images detected.`);
-    deletedImages.forEach((image) => log.warn(`  \u2716 ${actualDir}${image}`));
-  }
-
-  if (newImages.length > 0) {
-    log.info(`\n\u274B ${newImages.length} new images detected.`);
-    newImages.forEach((image) => log.info(`  \u271A ${actualDir}${image}`));
-  }
-
-  compareImages(expectedImages, actualImages)
-    .then((results) => {
-      const passed = results.filter(r => r.passed).map((r) => r.image);
-      const failed = results.filter(r => !r.passed).map((r) => r.image);
-
-      report({
-        passedItems: passed,
-        failedItems: failed,
-        newItems: newImages,
-        deletedItems: deletedImages,
-        dist: dist || './reg.json',
-        actualDir,
-        expectedDir,
-        diffDir,
-      });
-
-      spinner.stop(true);
-      if (passed.length > 0) {
-        log.success(`\n\u2714 ${passed.length} test succeeded.`);
-        passed.forEach((image) => {
-          try {
-            fs.unlinkSync(`${diffDir}${image}`);
-          } catch (err) {
-            // noop
-          }
-          log.success(`  \u2714 ${actualDir}${image}`);
-        });
-      }
-
-      if (failed.length > 0) {
-        log.fail(`\n\u2718 ${failed.length} test failed.`);
-        failed.forEach((image) => log.fail(`  \u2718 ${actualDir}${image}`));
-      }
-
-      if (!update) {
-        if (failed.length > 0 || newImages.length > 0 || deletedImages.length > 0) {
-          log.fail(`\nInspect your code changes, re-run with \`-U\` to update them. `);
-          if (!ignoreError) process.exit(1);
+    const copyImages = () => {
+      actualImages.forEach((image) => {
+        try {
+          mkdirp.sync(path.dirname(`${expectedDir}${image}`));
+          fs.createReadStream(`${actualDir}${image}`)
+            .pipe(fs.createWriteStream(`${expectedDir}${image}`));
+        } catch (err) {
+          log.fail(err);
         }
-      }
+      })
+    };
 
-      spinner.start();
-      cleanupExpectedDir();
-      copyImages();
-      log.success(`\nAll images are updated. `);
-      spinner.stop(true);
-      resolve();
-    })
-    .catch(err => {
-      log.fail(err);
-      process.exit(1);
-    });
-});
+    if (deletedImages.length > 0) {
+      log.warn(`\n\u274B ${deletedImages.length} deleted images detected.`);
+      deletedImages.forEach((image) => log.warn(`  \u2716 ${actualDir}${image}`));
+    }
+
+    if (newImages.length > 0) {
+      log.info(`\n\u274B ${newImages.length} new images detected.`);
+      newImages.forEach((image) => log.info(`  \u271A ${actualDir}${image}`));
+    }
+
+    compareImages(expectedImages, actualImages)
+      .then((results) => {
+        const passed = results.filter(r => r.passed).map((r) => r.image);
+        const failed = results.filter(r => !r.passed).map((r) => r.image);
+
+        report({
+          passedItems: passed,
+          failedItems: failed,
+          newItems: newImages,
+          deletedItems: deletedImages,
+          expectedItems: expectedImages,
+          dist: dist || './reg.json',
+          actualDir,
+          expectedDir,
+          diffDir,
+        });
+
+        spinner.stop(true);
+        if (passed.length > 0) {
+          log.success(`\n\u2714 ${passed.length} test succeeded.`);
+          passed.forEach((image) => {
+            try {
+              fs.unlinkSync(`${diffDir}${image}`);
+            } catch (err) {
+              // noop
+            }
+            log.success(`  \u2714 ${actualDir}${image}`);
+          });
+        }
+
+        if (failed.length > 0) {
+          log.fail(`\n\u2718 ${failed.length} test failed.`);
+          failed.forEach((image) => log.fail(`  \u2718 ${actualDir}${image}`));
+        }
+
+        if (!update) {
+          if (failed.length > 0 || newImages.length > 0 || deletedImages.length > 0) {
+            log.fail(`\nInspect your code changes, re-run with \`-U\` to update them. `);
+            if (!ignoreError) process.exit(1);
+          }
+        }
+
+        spinner.start();
+        cleanupExpectedDir();
+        copyImages();
+        log.success(`\nAll images are updated. `);
+        spinner.stop(true);
+        resolve();
+      })
+      .catch(err => {
+        log.fail(err);
+        process.exit(1);
+      });
+  });
 
