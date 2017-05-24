a1fd7038d49da2423722e7f753875f5477b33bb6
bokuweb <bokuweb12@gmail.com>
2017-05-24 12:35:56 +0900

    Add lazyload

--- report/src/main.js
+++ report/src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue';
+import VueLazyload from 'vue-lazyload';
 
 const App = require('./app.vue');
+
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  loading: 'assets/image.png',
+  error: 'assets/image.png',
+  adapter: {
+    loaded({ bindType, el, naturalHeight, naturalWidth, $parent, src, loading, error, Init }) {
+      console.log('loaded')
+    },
+    loading(listender, Init) {
+      console.log('loading')
+    },
+    error(listender, Init) {
+      console.log('error')
+    }
+  }
+});
 
 new Vue({
   el: '#app',
