2b59e6494f0b67732bf824eecb0920bc133fd1c5
bokuweb <bokuweb12@gmail.com>
2017-05-24 15:51:04 +0900

    Fix image

--- report/src/main.js
+++ report/src/main.js
@@ -7,8 +7,8 @@ const App = require('./App.vue');
 Vue.use(VueThinModal);
 Vue.use(VueLazyload, {
   preLoad: 1.3,
-  loading: 'assets/image.png',
-  error: 'assets/image.png',
+  loading: 'https://github.com/bokuweb/reg-cli/blob/master/docs/image.png?raw=true',
+  error: 'https://github.com/bokuweb/reg-cli/blob/master/docs/image.png?raw=true',
 });
 
 new Vue({
