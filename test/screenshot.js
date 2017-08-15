51c2433cc223b9982fecd2246b2ec6b6055aac2a
bokuweb <bokuweb12@gmail.com>
2017-08-15 19:25:56 +0900

    Add screenshot

---
+++ test/screenshot.js
@@ -0,0 +1,24 @@
+const Nightmare = require("nightmare");
+const nightmare = Nightmare({
+  show: false, width: 1200, height: 2400, webPreferences: {
+    nodeIntegration: true,
+  }
+});
+const mkdirp = require("mkdirp");
+const path = require("path");
+const root = path.resolve(__dirname, '..');
+
+mkdirp.sync(`${root}/screenshot/actual`);
+
+nightmare
+  .goto(`file://${root}/sample/index.html`)
+  .wait(1000)
+  .screenshot(`${root}/screenshot/index.png`)
+  .end()
+  .then(() => {
+    console.log("Captured screenshot")
+  })
+  .catch(x => {
+    console.error(x);
+    process.exit(1);
+  });
