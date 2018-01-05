2f40447d33e4e7db9b8cf1bf47a0d098eeec218a
bokuweb <bokuweb12@gmail.com>
2018-01-05 19:20:01 +0900

    Modify threshold

--- src/diff.js
+++ src/diff.js
@@ -1,33 +1,69 @@
 /* @flow */
 
-import { imgDiff } from 'img-diff-js'; // $FlowIgnore
-import md5File from 'md5-file'; // $FlowIgnore
+import { imgDiff } from "img-diff-js"; // $FlowIgnore
+import md5File from "md5-file"; // $FlowIgnore
 
 export type DiffCreatorParams = {
-  actualDir: string;
-  expectedDir: string;
-  diffDir: string;
-  image: string;
-  threshold: number;
-  enableAntialias: boolean;
-}
+  actualDir: string,
+  expectedDir: string,
+  diffDir: string,
+  image: string,
+  thresholdRate?: number,
+  thresholdPixel?: number,
+  enableAntialias: boolean
+};
 
 export type DiffResult = {
-  image: string;
-  passed: boolean;
-}
+  image: string,
+  passed: boolean
+};
 
-const getMD5 = (file) => new Promise((resolve, reject) => {
-  md5File(file, (err, hash) => {
-    if (err) reject(err);
-    resolve(hash);
-  })
-});
+const getMD5 = file =>
+  new Promise((resolve, reject) => {
+    md5File(file, (err, hash) => {
+      if (err) reject(err);
+      resolve(hash);
+    });
+  });
 
-const createDiff = ({ actualDir, expectedDir, diffDir, image, threshold, enableAntialias }: DiffCreatorParams) => {
+const isPassed = ({
+  width,
+  height,
+  diffCount,
+  thresholdPixel,
+  thresholdRate
+}: {
+  width: number,
+  height: number,
+  diffCount: number,
+  thresholdPixel?: number,
+  thresholdRate?: number
+}) => {
+  if (typeof thresholdPixel !== "undefined" && !Number.isNaN(thresholdPixel)) {
+    return diffCount <= thresholdPixel;
+  } else if (
+    typeof thresholdRate !== "undefined" &&
+    !Number.isNaN(thresholdPixel)
+  ) {
+    const totalPixel = width * height;
+    const ratio = diffCount / totalPixel;
+    return ratio <= thresholdRate;
+  }
+  return diffCount === 0;
+};
+
+const createDiff = ({
+  actualDir,
+  expectedDir,
+  diffDir,
+  image,
+  thresholdRate,
+  thresholdPixel,
+  enableAntialias
+}: DiffCreatorParams) => {
   return Promise.all([
     getMD5(`${actualDir}${image}`),
-    getMD5(`${expectedDir}${image}`),
+    getMD5(`${expectedDir}${image}`)
   ]).then(([actualHash, expectedHash]) => {
     if (actualHash === expectedHash) {
       if (!process || !process.send) return;
@@ -40,34 +76,22 @@ const createDiff = ({ actualDir, expectedDir, diffDir, image, threshold, enableA
       diffFilename: `${diffDir}${diffImage}`,
       options: {
         threshold: 0,
-        includeAA: !enableAntialias,
-      },
-    })
-      .then(({ width, height, diffCount }) => {
-        if (!process || !process.send) return;
-        const totalPixel = width * height;
-        const ratio = diffCount / totalPixel;
-        const passed = ratio <= threshold;
-        process.send({ passed, image });
-      })
-  })
+        includeAA: !enableAntialias
+      }
+    }).then(({ width, height, diffCount }) => {
+      const passed = isPassed({
+        width,
+        height,
+        diffCount,
+        thresholdPixel,
+        thresholdRate
+      });
+      if (!process || !process.send) return;
+      process.send({ passed, image });
+    });
+  });
 };
 
-process.on('message', (data) => {
+process.on("message", data => {
   createDiff(data);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/index.js
+++ src/index.js
@@ -1,120 +1,170 @@
 /* @flow */
 
-import glob from 'glob'; // $FlowIgnore
-import mkdirp from 'make-dir'; // $FlowIgnore
-import fs from 'fs';
-import path from 'path';
-import { range } from 'lodash';
-import log from './log';
-import createReport from './report';
-import bluebird from 'bluebird'; // $FlowIgnore
-import { EventEmitter } from 'events';
-import ProcessAdaptor from './process-adaptor';
-import type { DiffCreatorParams } from './diff';
+import glob from "glob"; // $FlowIgnore
+import mkdirp from "make-dir"; // $FlowIgnore
+import fs from "fs";
+import path from "path";
+import { range } from "lodash";
+import log from "./log";
+import createReport from "./report";
+import bluebird from "bluebird"; // $FlowIgnore
+import { EventEmitter } from "events";
+import ProcessAdaptor from "./process-adaptor";
+import type { DiffCreatorParams } from "./diff";
 
-const IMAGE_FILES = '/**/*.+(tiff|jpeg|jpg|gif|png|bmp)';
+const IMAGE_FILES = "/**/*.+(tiff|jpeg|jpg|gif|png|bmp)";
 
 type CompareResult = {
-  passed: boolean;
-  image: string;
+  passed: boolean,
+  image: string
 };
 
 type RegParams = {
-  actualDir: string;
-  expectedDir: string;
-  diffDir: string;
-  report?: string;
-  json?: string;
-  update?: boolean;
-  urlPrefix?: string;
-  threshold?: number;
-  concurrency?: number;
-  enableAntialias?: boolean;
-  enableClientAdditionalDetection?: boolean;
+  actualDir: string,
+  expectedDir: string,
+  diffDir: string,
+  report?: string,
+  json?: string,
+  update?: boolean,
+  urlPrefix?: string,
+  thresholdRate?: number,
+  thresholdPixel?: number,
+  concurrency?: number,
+  enableAntialias?: boolean,
+  enableClientAdditionalDetection?: boolean
 };
 
 const difference = (arrA, arrB) => arrA.filter(a => !arrB.includes(a));
 
 const copyImages = (actualImages, { expectedDir, actualDir }) => {
-  return Promise.all(actualImages.map((image) => new Promise((resolve, reject) => {
-    try {
-      mkdirp.sync(path.dirname(`${expectedDir}${image}`));
-      const writeStream = fs.createWriteStream(`${expectedDir}${image}`);
-      fs.createReadStream(`${actualDir}${image}`).pipe(writeStream);
-      writeStream.on('finish', (err) => {
-        if (err) reject(err);
-        resolve();
-      })
-    } catch (err) {
-      reject(err);
-    }
-  })))
+  return Promise.all(
+    actualImages.map(
+      image =>
+        new Promise((resolve, reject) => {
+          try {
+            mkdirp.sync(path.dirname(`${expectedDir}${image}`));
+            const writeStream = fs.createWriteStream(`${expectedDir}${image}`);
+            fs.createReadStream(`${actualDir}${image}`).pipe(writeStream);
+            writeStream.on("finish", err => {
+              if (err) reject(err);
+              resolve();
+            });
+          } catch (err) {
+            reject(err);
+          }
+        })
+    )
+  );
 };
 
-const compareImages = (emitter, {
-  expectedImages,
-  actualImages,
-  dirs,
-  threshold,
-  concurrency,
-  enableAntialias,
-}): Promise<CompareResult[]> => {
-  const images = actualImages.filter((actualImage) => expectedImages.includes(actualImage));
+const compareImages = (
+  emitter,
+  {
+    expectedImages,
+    actualImages,
+    dirs,
+    thresholdRate,
+    thresholdPixel,
+    concurrency,
+    enableAntialias
+  }
+): Promise<CompareResult[]> => {
+  const images = actualImages.filter(actualImage =>
+    expectedImages.includes(actualImage)
+  );
   concurrency = images.length < 20 ? 1 : concurrency || 4;
   const processes = range(concurrency).map(() => new ProcessAdaptor(emitter));
-  return bluebird.map(images, (image) => {
-    const p = processes.find(p => !p.isRunning());
-    if (p) {
-      return p.run({ ...dirs, image, threshold: threshold || 0, enableAntialias });
-    }
-  }, { concurrency }).then((result) => {
-    processes.forEach((p) => p.close());
-    return result;
-  }).filter(r => !!r);
+  return bluebird
+    .map(
+      images,
+      image => {
+        const p = processes.find(p => !p.isRunning());
+        if (p) {
+          return p.run({
+            ...dirs,
+            image,
+            thresholdPixel,
+            thresholdRate,
+            enableAntialias
+          });
+        }
+      },
+      { concurrency }
+    )
+    .then(result => {
+      processes.forEach(p => p.close());
+      return result;
+    })
+    .filter(r => !!r);
 };
 
 const cleanupExpectedDir = (expectedImages, expectedDir) => {
-  expectedImages.forEach((image) => fs.unlinkSync(`${expectedDir}${image}`));
+  expectedImages.forEach(image => fs.unlinkSync(`${expectedDir}${image}`));
 };
 
-const aggregate = (result) => {
-  const passed = result.filter(r => r.passed).map((r) => r.image);
-  const failed = result.filter(r => !r.passed).map((r) => r.image);
+const aggregate = result => {
+  const passed = result.filter(r => r.passed).map(r => r.image);
+  const failed = result.filter(r => !r.passed).map(r => r.image);
   const diffItems = failed.map(image => image.replace(/\.[^\.]+$/, ".png"));
   return { passed, failed, diffItems };
 };
 
-const updateExpected = ({ actualDir, expectedDir, diffDir, expectedItems, actualItems }) => {
+const updateExpected = ({
+  actualDir,
+  expectedDir,
+  diffDir,
+  expectedItems,
+  actualItems
+}) => {
   cleanupExpectedDir(expectedItems, expectedDir);
-  return copyImages(actualItems, { actualDir, expectedDir, diffDir }).then(() => {
-    log.success(`\nAll images are updated. `);
-  });
+  return copyImages(actualItems, { actualDir, expectedDir, diffDir }).then(
+    () => {
+      log.success(`\nAll images are updated. `);
+    }
+  );
 };
 
 module.exports = (params: RegParams) => {
-  const { actualDir, expectedDir, diffDir, json, concurrency, update,
-    report, urlPrefix, threshold, enableAntialias, enableClientAdditionalDetection } = params;
+  const {
+    actualDir,
+    expectedDir,
+    diffDir,
+    json,
+    concurrency,
+    update,
+    report,
+    urlPrefix,
+    thresholdRate,
+    thresholdPixel,
+    enableAntialias,
+    enableClientAdditionalDetection
+  } = params;
   const dirs = { actualDir, expectedDir, diffDir };
   const emitter = new EventEmitter();
-  const expectedImages = glob.sync(`${expectedDir}${IMAGE_FILES}`).map(path => path.replace(expectedDir, ''));
-  const actualImages = glob.sync(`${actualDir}${IMAGE_FILES}`).map(path => path.replace(actualDir, ''));
+  const expectedImages = glob
+    .sync(`${expectedDir}${IMAGE_FILES}`)
+    .map(path => path.replace(expectedDir, ""));
+  const actualImages = glob
+    .sync(`${actualDir}${IMAGE_FILES}`)
+    .map(path => path.replace(actualDir, ""));
   const deletedImages = difference(expectedImages, actualImages);
   const newImages = difference(actualImages, expectedImages);
 
   mkdirp.sync(expectedDir);
   mkdirp.sync(diffDir);
 
-  setImmediate(() => emitter.emit('start'));
+  setImmediate(() => emitter.emit("start"));
 
   compareImages(emitter, {
     expectedImages,
     actualImages,
     dirs,
-    threshold,
+    thresholdPixel,
+    thresholdRate,
     concurrency,
-    enableAntialias: !!enableAntialias,
+    enableAntialias: !!enableAntialias
   })
-    .then((result) => aggregate(result))
+    .then(result => aggregate(result))
     .then(({ passed, failed, diffItems }) => {
       return createReport({
         passedItems: passed,
@@ -125,30 +175,32 @@ module.exports = (params: RegParams) => {
         previousExpectedImages: expectedImages,
         actualItems: actualImages,
         diffItems,
-        json: json || './reg.json',
+        json: json || "./reg.json",
         actualDir,
         expectedDir,
         diffDir,
-        report: report || '',
-        urlPrefix: urlPrefix || '',
-        enableClientAdditionalDetection: !!enableClientAdditionalDetection,
+        report: report || "",
+        urlPrefix: urlPrefix || "",
+        enableClientAdditionalDetection: !!enableClientAdditionalDetection
       });
     })
-    .then((result) => {
-      deletedImages.forEach((image) => emitter.emit('compare', { type: 'delete', path: image }));
-      newImages.forEach((image) => emitter.emit('compare', { type: 'new', path: image }));
+    .then(result => {
+      deletedImages.forEach(image =>
+        emitter.emit("compare", { type: "delete", path: image })
+      );
+      newImages.forEach(image =>
+        emitter.emit("compare", { type: "new", path: image })
+      );
       if (update) {
-        return updateExpected(result)
-          .then(() => {
-            emitter.emit('update');
-            return result
-          });
+        return updateExpected(result).then(() => {
+          emitter.emit("update");
+          return result;
+        });
       }
       return result;
     })
-    .then((result) => emitter.emit('complete', result))
-    .catch(err => emitter.emit('error', err));
+    .then(result => emitter.emit("complete", result))
+    .catch(err => emitter.emit("error", err));
 
   return emitter;
 };
-
