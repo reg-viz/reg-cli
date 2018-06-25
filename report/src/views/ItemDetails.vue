783ad89f99764c8dd5f90cb534d07577e80454a9
bokuweb <bokuweb12@gmail.com>
2018-06-25 21:58:32 +0900

    fix: fix image link

--- report/src/views/ItemDetails.vue
+++ report/src/views/ItemDetails.vue
@@ -5,14 +5,14 @@
                 <i :class="'ui icon ' + icon"></i>{{item.raw}}
             </a>
             <div class="captures">
-                <div v-if="diffDir" class="capture" v-on:click="openCapture(diffDir + item.raw, actualDir + item.raw)">
-                    <capture-image :src="diffDir + item.raw" :kind="'Diff'"></capture-image>
+                <div v-if="diffDir" class="capture" v-on:click="openCapture(createSrc(diffDir, item.raw), createSrc(actualDir, item.raw))">
+                    <capture-image :src="createSrc(diffDir, item.raw)" :kind="'Diff'"></capture-image>
                 </div>
                 <div v-if="actualDir" class="capture" v-on:click="open(item.raw, actualDir)">
-                    <capture-image :src="actualDir + item.raw" :kind="'After'"></capture-image>
+                    <capture-image :src="createSrc(actualDir, item.raw)" :kind="'After'"></capture-image>
                 </div>
                 <div v-if="expectedDir" class="capture" v-on:click="open(item.raw, expectedDir)">
-                    <capture-image :src="expectedDir + item.raw" :kind="'Before'"></capture-image>
+                    <capture-image :src="createSrc(expectedDir, item.raw)" :kind="'Before'"></capture-image>
                 </div>
             </div>
         </div>
@@ -20,23 +20,27 @@
 </template>
 
 <script>
+const path = require('path');
 const CaptureImage = require('./CaptureImage.vue');
 
 module.exports = {
-    name: 'ItemDetails',
-    components: {
-        'capture-image': CaptureImage,
-    },
-    props: ['items', 'color', 'icon', 'openCapture', 'openComparison', 'diffDir', 'actualDir', 'expectedDir'],
-    methods: {
-      open: function(src, dir) {
-        if (this.openComparison) {
-          this.openComparison(src);
-        } else {
-          this.openCapture(dir + src);
-        }
+  name: 'ItemDetails',
+  components: {
+      'capture-image': CaptureImage,
+  },
+  props: ['items', 'color', 'icon', 'openCapture', 'openComparison', 'diffDir', 'actualDir', 'expectedDir'],
+  methods: {
+    open: function(src, dir) {
+      if (this.openComparison) {
+        this.openComparison(src);
+      } else {
+        this.openCapture(this.createSrc(dir, src));
       }
     },
+    createSrc: function(dir, file) {
+      return path.join(dir || '', file || '');
+    },
+  },
 }
 </script>
 
