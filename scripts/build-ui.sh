1f243d6db9f6e23875697018e8f76e3948a673f5
tsuyoshi wada <wadackel@gmail.com>
2021-09-01 00:27:24 +0900

    chore: Fix UI build scripts (#402)

--- scripts/build-ui.sh
+++ scripts/build-ui.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+
 VERSION=$1
 DIR=report/ui
 
+set -eux
+
 if [ -e "$DIR" ]; then
   cd $DIR
-  git fetch origin
+  git fetch origin --tags
   git checkout refs/tags/$VERSION
 else
   git clone https://github.com/reg-viz/reg-cli-report-ui.git -b $VERSION $DIR --depth 1
@@ -16,6 +19,3 @@ yarn install --frozen-lockfile
 yarn build
 
 touch .npmignore
-
-
-
