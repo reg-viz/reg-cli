6adbc314e57ef1acdca26a0b75f3db5988ffa035
bokuweb <bokuweb12@gmail.com>
2024-09-30 08:59:34 +0900

    wip

--- js/build.config.ts
+++ js/build.config.ts
@@ -1,7 +1,7 @@
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
-  entries: ['./index.ts', './cli.ts', './worker.ts', './entry.ts', './proxy.ts'],
+  entries: ['./index.ts', './cli.ts', './worker.ts', './entry.ts'],
   declaration: true,
   rollup: {
     emitCJS: true,
