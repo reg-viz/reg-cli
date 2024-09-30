b1dfffe58e031665532864e61d8d37279fb36a98
bokuweb <bokuweb12@gmail.com>
2024-09-30 11:35:51 +0900

    wip

--- js/utils.ts
+++ js/utils.ts
@@ -3,15 +3,15 @@ import { fileURLToPath } from 'url';
 import { readFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
-const isESM = typeof import.meta !== 'undefined';
+const isCJS = typeof __dirname !== 'undefined';
 
 export const readWasm = () => {
-  const dir = isESM ? path.dirname(fileURLToPath(import.meta.url)) : __dirname;
+  const dir = isCJS ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
   const file = readFile(join(dir, './reg.wasm'));
   return file;
 };
 
 export const resolveExtention = (): string => {
-  return isESM ? 'mjs' : 'cjs';
+  return isCJS ? 'cjs' : 'mjs';
 };
